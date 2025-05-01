e769f9fa3ec837140d0e204a005224cdd5ab252a
WineJay <hossaina3745@masseyhigh.school.nz>
2025-05-02 09:10:12 +1200

    Add files via upload
    updated.

--- B_01_recipe_cost.v2.py
+++ B_01_recipe_cost.v2.py
@@ -1,7 +1,15 @@
-import pandas
+# import pandas
+# import math
 
 
 # Functions go here
+
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
 
 def num_check(question, num_type="float"):
     """checks that the user enter th full word
@@ -27,6 +35,42 @@ def num_check(question, num_type="float"):
 
         except ValueError:
             print(error)
+
+
+def int_check(question):
+    """checks that the user enter an integer. """
+
+    error = "Oops - please enter an integer. "
+
+    while True:
+        try:
+            # Return the response if it's an integer
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print(error)
+
+
+def instructions():
+    make_statement("Instructions", "ℹ️")
+    """Instructions for using Recipe Cost Calculator"""
+
+    print('''
+
+For each recipe enter...
+- Ingredient name
+- The amount
+- How much did you buy?
+- Price for one
+
+The program will first record the price you spent on the ingredient then the total spent then at last ingredient per 
+serving.
+
+Once you have entered the exit code ('xxx'), the program will display the ingredient
+information and the total cost.
+
+    ''')
 
 
 def not_blank(question):
@@ -62,14 +106,25 @@ def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
         print(f"Please choose an option from {valid_answers}")
 
 
-# dictionary
-all_paid = []
-all_ingredient = []
+def unit_check(question, valid_answers=('kg', 'g', 'grams', '')):
+    while True:
 
-recipe_detail_dict = {
-    'total spend': all_paid,
-    'Ingredient': all_ingredient
-}
+        response = input(question).lower()
+
+        for entry in valid_answers:
+
+            # check if the response is the entire word
+            if response == entry:
+                return entry
+
+
+# main routine Goes here
+make_statement(f"Recipe Cost", "🍳")
+print()
+
+want_instructions = string_check("Do you want to see the instructions?")
+if want_instructions == "yes":
+    instructions()
 
 # loop for testing purposes
 
@@ -82,9 +137,13 @@ while True:
     # if name is exit code, break out of loop
     if ingredient == "xxx":
         break
-    amount = num_check(f"How much are you using (in grams or no unit): ")
 
-    brought = num_check(f"How much did you buy (also in grams or no units): ")
+        # using units
+    units = unit_check(f"What is your units for this ingredient: ")
+
+    amount = num_check(f"How much are you using (in your units): ")
+
+    brought = num_check(f"How much did you buy (also in your units): ")
 
     price = num_check(f"How much did you pay?: ")
 
@@ -93,20 +152,10 @@ while True:
         print(f"You haven't brought enough")
         break
 
-    all_paid.append(price)
-
     # find how much per serving for each ingredient
-    ingredient_cost = (price / brought) * amount
+    each_ingredient_per_dollar = (price / brought)
 
     # calculate the total spend
-    print(f"You have brought ingredient worth of", ingredient_cost, f"(It is calculated on per "
-                                                                    f"item or gram per  dollar)")
-# panda
-recipe_details_frame = pandas.DataFrame(recipe_detail_dict)
-
-# total
-total_paid = recipe_details_frame['total spend'].sum()
-
-print(recipe_details_frame.to_string(index=False))
-print()
-print(F"you have spend: ${total_paid:.2f}")
+    print(f"You have brought ingredient worth of", each_ingredient_per_dollar * amount, f"(It is calculated on the "
+                                                                                        f"units you have entered)")
+    print()
---
+++ B_01_recipe_cost.v4.py
@@ -0,0 +1,176 @@
+# import pandas
+# import math
+
+
+# Functions go here
+
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+def num_check(question, num_type="float"):
+    """checks that the user enter th full word
+    or the first letter of a word from a list of valid response"""
+
+    if num_type == "float":
+        error = "Please enter an number more than 0"
+    else:
+        error = "Please enter an integer more than 0"
+
+    while True:
+        try:
+
+            if num_type == "float":
+                response = float(input(question))
+            else:
+                response = int(input(question))
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+def int_check(question):
+    """checks that the user enter an integer. """
+
+    error = "Oops - please enter an integer. "
+
+    while True:
+        try:
+            # Return the response if it's an integer
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print(error)
+
+
+def instructions():
+    make_statement("Instructions", "ℹ️")
+    """Instructions for using Recipe Cost Calculator"""
+
+    print('''
+
+For each recipe enter...
+- Ingredient name
+- The units
+- The amount
+- How much did you buy?
+- Price you paid
+
+The program will first record the price you spent on the ingredient then the total spent then at last ingredient per 
+serving.
+
+Once you have entered the exit code ('xxx'), the program will display the ingredient
+information and the total cost.
+
+    ''')
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again.\n")
+
+
+def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
+    """checks that the user enter th full word
+    or the first letter of a word from a list of valid response"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for entry in valid_answers:
+
+            # check if the response is the entire word
+            if response == entry:
+                return entry
+
+            # check if it's the first letter
+            elif response == entry[:num_letters]:
+                return entry
+
+        print(f"Please choose an option from {valid_answers}")
+
+
+def unit_check(question, valid_answers=('kg', 'kilo', 'g', 'grams', 'mL', 'L', 'ml', '')):
+    while True:
+
+        response = input(question).lower()
+
+        for entry in valid_answers:
+
+            # check if the response is the entire word
+            if response == entry:
+                return entry
+
+
+# defaulting kilo to grams
+default_kilo_to = 0
+
+# main routine Goes here
+make_statement(f"Recipe Cost", "🍳")
+print()
+
+want_instructions = string_check("Do you want to see the instructions?")
+if want_instructions == "yes":
+    instructions()
+
+# loop for testing purposes
+
+recipe_name = not_blank("Recipe Name: ")
+serving_size = num_check("Serving:", "integer")
+print(f"You are making {serving_size} {recipe_name}")
+print()
+while True:
+    ingredient = not_blank(f"Ingredient: ")
+    # if name is exit code, break out of loop
+    if ingredient == "xxx":
+        break
+
+        # using units
+    amount = num_check(f"How much are you using: ")
+
+    units_used = unit_check(f"What is your units for this ingredient: ")
+
+    brought = num_check(f"How much did you buy : ")
+
+    units_brought = unit_check(f"What is your units for this ingredient: ")
+
+    price = num_check(f"How much did you pay?: ")
+
+    # find how much per serving for each ingredient
+    each_ingredient_per_dollar = (price / brought)
+
+    if units_brought == "kg":
+        each_ingredient_per_dollar = (price / (brought * 1000))
+
+    if units_brought == "kilo":
+        each_ingredient_per_dollar = (price / (brought * 1000))
+
+    if units_brought == "L":
+        each_ingredient_per_dollar = (price / (brought * 1000))
+
+    # calculate the total spend
+    print(f"You have got ingredient worth of $", each_ingredient_per_dollar * amount)
+
+    # output error message / success message
+make_statement("Ingredient Cost", "🍳")
+
+to_write = []
+
+print()
